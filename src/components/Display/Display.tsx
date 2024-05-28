cd1fc87c0eb823903236d997d14ce7b05396c4d0
kod-python <kelvinlik0245@gmail.com>
2024-05-28 17:28:56 +0000

    kod commit

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,10 +1,5 @@
-
-
 export const data = [
-  { name: "John", age: 30, password: "hey you" },
-  { name: "Alice", age: 20, password: "3345" },
-  { name: "Bob", age: 20, password: "1234", imageUrl:"https://buffer.com/library/content/images/2023/10/free-images.jpg" },
+  { name: "John", age: 30, password: "hey you", imageUrl:"https://www.wbcsd.org/var/site/storage/images/media/page-assets/imperative/nature-action/nature-action-intro/243120-2-eng-GB/Nature-action-intro_i1140.jpg" },
+  { name: "Alice", age: 20, password: "3345", imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxljCiU3pRUXpw-39aklTNk7BDV3G9Dn7ocw&s" },
+  { name: "Bob", age: 20, password: "1234", imageUrl: "https://buffer.com/library/content/images/2023/10/free-images.jpg" },
 ];
-
-
-let newArray = {name:"ben", age:25, password:""}
